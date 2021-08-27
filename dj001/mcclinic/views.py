7d5f6d70d39d4292ae722384cb1ef38c5e2bdb0b
reedjohnnny <31574677+reedjohnnny@users.noreply.github.com>
2021-08-27 19:40:13 +0300

    27/08/2021 -2

--- dj001/mcclinic/views.py
+++ dj001/mcclinic/views.py
@@ -9,10 +9,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+#Paginator
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.template import RequestContext
 
 
 def main_page(request):
-    return render(request, 'main/main_page.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'main/main_page.html', {'posts': posts})
 
 
 def post_list(request):
@@ -64,6 +68,22 @@ urlpatterns = [
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
+#Paginator
 
+def news(request):
+    '''Show all news'''
+    posts_list = Post.objects.all().order_by('-timestamp')
+    paginator = Paginator(posts_list, 8)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    vars = dict(
+        posts=posts,
+        )
+    return render('news.html', vars, context_instance=RequestContext(request))
 
 

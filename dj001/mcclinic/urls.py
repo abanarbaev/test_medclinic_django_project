8b37cbc97dea6c3ae5053284232fe9c5cabf5db7
reedjohnnny <31574677+reedjohnnny@users.noreply.github.com>
2021-09-17 19:26:24 +0300

    17/09/21 analyzes

--- dj001/mcclinic/urls.py
+++ dj001/mcclinic/urls.py
@@ -10,4 +10,14 @@ urlpatterns = [
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('about/doctors', views.doctors, name='doctors'),
     path('about/doctors/doctor_page/', views.doctor_page, name='doctor_page'),
+    path('about/contacts', views.contacts, name='contacts'),
+    path('about/docsl', views.docsl, name='docsl'),
+    path('about/insurance', views.insurance, name='insurance'),
+    path('about/job', views.job, name='job'),
+    path('about/promo', views.promo, name='promo'),
+    path('about/services', views.services, name='services'),
+    path('about/analyzes', views.analyzes, name='analyzes'),
+    path('about/analyzes_price', views.analyzes_price, name='analyzes_price'),
+    
+        
 ]
--- dj001/mcclinic/views.py
+++ dj001/mcclinic/views.py
@@ -97,6 +97,59 @@ def doctors(request):
 def doctor_page(request):
     return render(request, 'main/doctor_page.html', {})
 
+#contacts
+
+def contacts(request):
+    return render(request, 'main/contacts.html', {})
+
+#docsl
+
+def docsl(request):
+    return render(request, 'main/docsl.html', {})
+
+#insurance
+
+def insurance(request):
+    return render(request, 'main/insurance.html', {})
+
+#job
+
+def job(request):
+    return render(request, 'main/job.html', {})
+
+#promo
+
+def promo(request):
+    return render(request, 'main/promo.html', {})
+
+#services
+
+def services(request):
+    return render(request, 'main/services.html', {})
+
+#analyzes
+
+def analyzes(request):
+    return render(request, 'main/analyzes.html', {})
+
+#analyzes_price
+
+def analyzes_price(request):
+    object_list = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    paginator = Paginator(object_list, 10)  # 10 поста на каждой странице  
+    page = request.GET.get('page')  
+    try:  
+        posts = paginator.page(page)  
+    except PageNotAnInteger:  
+        # Если страница не является целым числом, поставим первую страницу  
+        posts = paginator.page(1)  
+    except EmptyPage:  
+        # Если страница больше максимальной, доставить последнюю страницу результатов  
+        posts = paginator.page(paginator.num_pages)  
+    return render(request, 'main/allclinic.html', {'page': page,'posts': posts})    
+
+
+
 
 
 
